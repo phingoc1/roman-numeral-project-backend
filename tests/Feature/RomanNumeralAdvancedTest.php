e0cc60693ff9cc7570d21bae89a7e8165ff5c36c
Phi <phingoc88@hotmail.com>
2023-04-30 00:39:21 +0200

    Shortened code and fixed typo in comment

---
+++ tests/Feature/RomanNumeralAdvancedTest.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RomanNumeralAdvancedTest extends TestCase
+{
+    public function test_advanced_roman_numeral_conversion_fail_if_missing_data()
+    {
+        $this->json('POST', 'api/roman-numerals-advanced', [], ['Accept' => 'application/json'])
+            ->assertStatus(400)
+            ->assertJson([
+                'status' => 'error',
+            ]);
+    }
+
+    public function test_advanced_roman_numeral_conversion_fail_if_contains_invalid_characters()
+    {
+        $data = [
+            'romanNumeralString' => 'cccwxlvi',
+        ];
+
+        $this->json('POST', 'api/roman-numerals-advanced', $data, ['Accept' => 'application/json'])
+            ->assertStatus(400)
+            ->assertJson([
+                'status' => 'error',
+            ]);
+    }
+
+    public function test_advanced_roman_numeral_conversion_fail_if_characters_invalid_order()
+    {
+        $data = [
+            'romanNumeralString' => 'iiix',
+        ];
+
+        $this->json('POST', 'api/roman-numerals-advanced', $data, ['Accept' => 'application/json'])
+            ->assertStatus(400)
+            ->assertJson([
+                'status' => 'error'
+            ]);
+    }
+
+    public function test_advanced_roman_numeral_conversion_iv()
+    {
+        $data = [
+            'romanNumeralString' => 'iv'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-advanced', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 4,
+            ]);
+    }
+
+    public function test_advanced_roman_numeral_conversion_xii()
+    {
+        $data = [
+            'romanNumeralString' => 'xii'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-advanced', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 12,
+            ]);
+    }
+
+    public function test_advanced_roman_numeral_conversion_xiv()
+    {
+        $data = [
+            'romanNumeralString' => 'xiv'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-advanced', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 14,
+            ]);
+    }
+
+    public function test_advanced_roman_numeral_conversion_ivc()
+    {
+        $data = [
+            'romanNumeralString' => 'ivc'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-advanced', $data, ['Accept' => 'application/json'])
+            ->assertStatus(400)
+            ->assertJson([
+                'status' => 'error',
+            ]);
+    }
+
+    public function test_advanced_roman_numeral_conversion_cccxlvi()
+    {
+        $data = [
+            'romanNumeralString' => 'cccxlvi'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-advanced', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 346,
+            ]);
+    }
+
+    public function test_advanced_roman_numeral_conversion_mix()
+    {
+        $data = [
+            'romanNumeralString' => 'mix'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-advanced', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 1009,
+            ]);
+    }
+
+
+}
---
+++ tests/Feature/RomanNumeralEasyTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RomanNumeralEasyTest extends TestCase
+{
+    public function test_easy_roman_numeral_conversion_missing_data()
+    {
+        $this->json('POST', 'api/roman-numerals-easy', [], ['Accept' => 'application/json'])
+            ->assertStatus(400)
+            ->assertJson([
+                'status' => 'error',
+            ]);
+    }
+
+    public function test_easy_roman_numeral_conversion_long_string()
+    {
+        $data = [
+            'romanNumeralString' => 'xii',
+        ];
+
+        $this->json('POST', 'api/roman-numerals-easy', $data, ['Accept' => 'application/json'])
+            ->assertStatus(400)
+            ->assertJson([
+                'status' => 'error',
+            ]);
+    }
+
+    public function test_easy_roman_numeral_conversion_invalid_characters()
+    {
+        $data = [
+            'romanNumeralString' => 'w',
+        ];
+
+        $this->json('POST', 'api/roman-numerals-easy', $data, ['Accept' => 'application/json'])
+            ->assertStatus(400)
+            ->assertJson([
+                'status' => 'error',
+            ]);
+    }
+
+    public function test_easy_roman_numeral_conversion_i()
+    {
+        $data = [
+            'romanNumeralString' => 'i'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-easy', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 1,
+            ]);
+    }
+
+    public function test_easy_roman_numeral_conversion_v()
+    {
+        $data = [
+            'romanNumeralString' => 'v'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-easy', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 5,
+            ]);
+    }
+
+    public function test_easy_roman_numeral_conversion_x()
+    {
+        $data = [
+            'romanNumeralString' => 'x'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-easy', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 10,
+            ]);
+    }
+
+    public function test_easy_roman_numeral_conversion_l()
+    {
+        $data = [
+            'romanNumeralString' => 'l'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-easy', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 50,
+            ]);
+    }
+
+    public function test_easy_roman_numeral_conversion_c()
+    {
+        $data = [
+            'romanNumeralString' => 'c'
+        ];
+
+        $this->json('POST', 'api/roman-numerals-easy', $data, ['Accept' => 'application/json'])
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => 'ok',
+                'payload' => 100,
+            ]);
+    }
+}
