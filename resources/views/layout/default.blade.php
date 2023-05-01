974ce7a42bfe8374e4ffe2a043c5884aaea07097
Phi <phingoc88@hotmail.com>
2023-05-01 13:22:25 +0200

    Extracted regex to consts

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -5,7 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Case task, UKE </title>
         <link rel="stylesheet" href="https://ukeweb-styleguide-cdn.s3.eu-central-1.amazonaws.com/0.99.105/osg.css">
-        <link rel="icon" href="http://localhost:8000/favicon.png">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
         <style>
             body {
                 display: flex;
